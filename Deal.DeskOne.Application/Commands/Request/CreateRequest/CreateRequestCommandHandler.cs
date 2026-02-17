47c0c6f9f38cbf0e23cd28309f6581374d033a81
José Lucas Sousa Ferreira <j.lucassousafer@gmail.com>
2026-02-17 15:21:09 -0300

    feat: implementa CreateRequest command e handler

--- Deal.DeskOne.Application/Commands/Request/CreateRequest/CreateRequestCommandHandler.cs
+++ Deal.DeskOne.Application/Commands/Request/CreateRequest/CreateRequestCommandHandler.cs
@@ -5,22 +5,23 @@ using Deal.DeskOne.Domain.Aggregates.Request;
 
 namespace Deal.DeskOne.Application.Commands.Request.CreateRequest
 {
-    public class CreateRequestCommandHandler(IRequestRepository requestsRepository, IUnitOfWork unitOfWork) : ICommandHandler<CreateRequestCommand>
+    public class CreateRequestCommandHandler(IRequestRepository requestsRepository, IUnitOfWork unitOfWork) : ICommandHandler<CreateRequestCommand, Guid>
     {
-        public async Task HandleAsync(CreateRequestCommand command, CancellationToken cancellationToken = default)
+        public async Task<Guid> HandleAsync(CreateRequestCommand command, CancellationToken cancellationToken = default)
         {
-
             var request = RequestAggregate.Create(
                 command.Title,
                 command.Description,
                 command.Category,
                 command.Priority,
                 command.CreatedBy
-                );
+            );
 
             await requestsRepository.AddAsync(request, cancellationToken);
 
             await unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return request.Id;
         }
     }
 }
