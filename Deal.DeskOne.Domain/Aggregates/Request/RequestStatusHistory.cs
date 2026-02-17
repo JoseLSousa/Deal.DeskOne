b9d22cbb889353a94c28d2dd641f445ab58c2cb2
José Lucas Sousa Ferreira <j.lucassousafer@gmail.com>
2026-02-17 15:40:13 -0300

    feat: adiciona entidade RequestStatusHistory para rastreamento de mudanças de status

---
+++ Deal.DeskOne.Domain/Aggregates/Request/RequestStatusHistory.cs
@@ -0,0 +1,33 @@
+﻿namespace Deal.DeskOne.Domain.Aggregates.Request
+{
+    public sealed class RequestStatusHistory
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public Guid RequestId { get; private set; }
+        public RequestStatus FromStatus { get; private set; }
+        public RequestStatus ToStatus { get; private set; }
+        public Guid ChangedBy { get; private set; }
+        public DateTime ChangedAt { get; private set; }
+        public string? Comment { get; private set; }
+
+        private RequestStatusHistory() { }
+
+        public static RequestStatusHistory Create(
+            Guid requestId,
+            RequestStatus fromStatus,
+            RequestStatus toStatus,
+            Guid changedBy,
+            string? comment = null)
+        {
+            return new RequestStatusHistory
+            {
+                RequestId = requestId,
+                FromStatus = fromStatus,
+                ToStatus = toStatus,
+                ChangedBy = changedBy,
+                ChangedAt = DateTime.UtcNow,
+                Comment = comment
+            };
+        }
+    }
+}
